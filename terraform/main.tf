b73d0cd91e8c832702f9c0d456c42a59c16909d9
Lasse Heia <23742718+lasseheia@users.noreply.github.com>
2023-09-17 11:28:06 +0200

    Add records for Office365 Exchange to heiaconsulting.no domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,6 +19,37 @@ resource "azurerm_dns_txt_record" "heia_consulting_no_office365_verification" {
   }
 }
 
+resource "azurerm_dns_mx_record" "heia_consulting_no_office365_email" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.heia_consulting_no.name
+  resource_group_name = azurerm_resource_group.dns_zones.name
+  ttl                 = 3600
+
+  record {
+    preference = 0
+    exchange   = "heiaconsulting-no.mail.protection.outlook.com"
+  }
+}
+
+resource "azurerm_dns_cname_record" "heia_consulting_no_office365_email" {
+  name                = "autodiscover"
+  zone_name           = azurerm_dns_zone.heia_consulting_no.name
+  resource_group_name = azurerm_resource_group.dns_zones.name
+  ttl                 = 3600
+  record              = "autodiscover.outlook.com"
+}
+
+resource "azurerm_dns_txt_record" "heia_consulting_no_office365_email" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.heia_consulting_no.name
+  resource_group_name = azurerm_resource_group.dns_zones.name
+  ttl                 = 3600
+
+  record {
+    value = "v=spf1 include:spf.protection.outlook.com -all"
+  }
+}
+
 resource "azurerm_dns_zone" "heia_consulting_com" {
   name                = "heiaconsulting.com"
   resource_group_name = azurerm_resource_group.dns_zones.name
